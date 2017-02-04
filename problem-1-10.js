0813f858217ec96b3f9de241dfafee157a19b594
Chris Lormor <cdlormor@gmail.com>
2017-02-04 18:18:55 +0000

    fixing indenting

--- problem-1-10.js
+++ problem-1-10.js
@@ -7,8 +7,8 @@ sumMultiplesOf3And5 = function() {
 
 	for (var i = 1; i < max; i++) {
 		if (i%3 == 0 || i%5 == 0) {
-    		result += i
-	    }
+			result += i
+		}
 	}
 	
 
@@ -23,7 +23,7 @@ var sumEvenFibonacci = function() {
 	var max = 4000000
 
 	for (var i = 1, j = 2, t = 0; i <= max ; t=i, i=j, j+=t) {
-    
+		
 		if (i%2 == 0) {
 			result += i
 		}
@@ -54,28 +54,28 @@ var largestPrimeFactor = function() {
 var largest3DigitPallindromeProject = function() {
 
 	var min = 100
-    var max = 999
+	var max = 999
 	var result = 0
-    
+	
 	for (var x = min; x <= max; x++) {
-    	for (var y = min; y <= max; y++) {
-        	if (isPallindrome(x*y) && result < (x*y)) {
-            	result = (x*y)
-            }
-    	}
-    }
+		for (var y = min; y <= max; y++) {
+			if (isPallindrome(x*y) && result < (x*y)) {
+				result = (x*y)
+			}
+		}
+	}
 
-    return result
+	return result
 }
 
 var isPallindrome = function(n) {
 	var numberString = ''+n
-    
-    for (var x=0,y=numberString.length - 1;x<y;x++,y--) {
-    	if (numberString[x] !== numberString[y]) {
-        	return false
-        }
-    }
+	
+	for (var x=0,y=numberString.length - 1;x<y;x++,y--) {
+		if (numberString[x] !== numberString[y]) {
+			return false
+		}
+	}
 
 	return true
 }
